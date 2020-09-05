8287c7cc273fbae4b705e96375cb429513f6df1f
OMPRAKASH MISHRA <omprakashmishra3978@gmail.com>
2020-09-04 17:25:25 -0700

    code changes to make more efficient subsequent runs

--- api/src/main/java/ca/bc/gov/educ/api/pendemog/migration/service/PenDemographicsMigrationService.java
+++ api/src/main/java/ca/bc/gov/educ/api/pendemog/migration/service/PenDemographicsMigrationService.java
@@ -133,13 +133,12 @@ public class PenDemographicsMigrationService implements Closeable {
   private List<Future<Boolean>> processDemog(String surNameLike) {
     List<Future<Boolean>> futures = new ArrayList<>();
     log.info("Now Processing surname starting with :: {}", surNameLike);
-    List<PenDemographicsEntity> penDemographicsEntities = getPenDemographicsMigrationRepository().findByStudSurnameLike(surNameLike + "%");
     List<StudentEntity> studentEntities = getStudentRepository().findByLegalLastNameLike(surNameLike + "%");
-    List<PenDemographicsEntity> penDemographicsEntitiesToBeProcessed = penDemographicsEntities.stream().filter(penDemographicsEntity ->
-        studentEntities.stream().allMatch(studentEntity -> (!penDemographicsEntity.getStudNo().trim().equals(studentEntity.getPen())))).collect(Collectors.toList());
-    log.info("Found {} records for surname starting with {}", penDemographicsEntitiesToBeProcessed.size(), surNameLike);
-    if (!penDemographicsEntitiesToBeProcessed.isEmpty()) {
-      final Callable<Boolean> callable = () -> studentService.processDemographicsEntities(penDemographicsEntitiesToBeProcessed, surNameLike);
+    var penNumList = studentEntities.stream().map(StudentEntity::getPen).collect(Collectors.toList());
+    List<PenDemographicsEntity> penDemographicsEntities = getPenDemographicsMigrationRepository().findByStudSurnameLikeAndStudNoNotIn(surNameLike + "%", penNumList);
+    log.info("Found {} records for surname starting with {}", penDemographicsEntities.size(), surNameLike);
+    if (!penDemographicsEntities.isEmpty()) {
+      final Callable<Boolean> callable = () -> studentService.processDemographicsEntities(penDemographicsEntities, surNameLike);
       futures.add(executorService.submit(callable));
     }
     return futures;
