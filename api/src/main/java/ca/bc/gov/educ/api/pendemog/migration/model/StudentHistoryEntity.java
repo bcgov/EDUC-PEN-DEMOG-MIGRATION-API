9b52ddd186f20521c431f686970851f83dc95084
OMPRAKASH MISHRA <omprakashmishra3978@gmail.com>
2020-12-24 14:10:50 -0800

    code changes for audit data migration

--- api/src/main/java/ca/bc/gov/educ/api/pendemog/migration/model/StudentHistoryEntity.java
+++ api/src/main/java/ca/bc/gov/educ/api/pendemog/migration/model/StudentHistoryEntity.java
@@ -13,7 +13,7 @@ import java.time.LocalDateTime;
 import java.util.UUID;
 
 @Entity
-@Table(name = "STUDENT_HISTORY")
+@Table(name = "API_STUDENT.STUDENT_HISTORY")
 @Data
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class StudentHistoryEntity {
